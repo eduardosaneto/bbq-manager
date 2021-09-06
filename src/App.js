82a115beb6d8ec9b092c6517fb5525560e8efb72
Eduardo Sá Neto <eduardo.albuquerque94@gmail.com>
2021-09-06 00:38:09 -0300

    feat: signin fully implemented

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { useState } from "react";
 import { ToastContainer } from "react-toastify";
 import GlobalStyle from "./assets/styles/GlobalStyle";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
+import UserContext from "./context/UserContext";
 
 function App() {
+  const [user, setUser] = useState("");
+
   return (
     <>
-      <GlobalStyle />
-      <Router>
-        <ToastContainer autoClose={2000} closeOnClick={true} />
-        <Switch>
-          <Route path="/" exact>
-            <SignIn />
-          </Route>
-          <Route path="/sign-up" exact>
-            <SignUp />
-          </Route>
-        </Switch>
-      </Router>
+      <UserContext.Provider value={{ user, setUser }}>
+        <GlobalStyle />
+        <Router>
+          <ToastContainer autoClose={2000} closeOnClick={true} />
+          <Switch>
+            <Route path="/" exact>
+              <SignIn />
+            </Route>
+            <Route path="/sign-up" exact>
+              <SignUp />
+            </Route>
+          </Switch>
+        </Router>
+      </UserContext.Provider>
     </>
   );
 }
--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,5 +1,5 @@
 import { useLocation, useHistory, Link } from "react-router-dom";
-import { useState } from "react";
+import { useState, useContext } from "react";
 import axios from "axios";
 import { toast } from "react-toastify";
 
@@ -8,15 +8,19 @@ import Background from "../../components/Background";
 import Input from "../../components/Form/Input";
 import Button from "../../components/Form/Button";
 import { Form, Row } from "../../components/Auth";
+import UserContext from "../../context/UserContext";
 
 export default function SignIn() {
   const location = useLocation();
   const history = useHistory();
+  const { setUser } = useContext(UserContext);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [loading, setLoading] = useState(false);
 
   const close = { autoClose: 2000 };
+
+  if (localStorage.length !== 0) history.push("/barbecues");
 
   const body = {
     email,
@@ -28,12 +32,14 @@ export default function SignIn() {
     setLoading(true);
     const request = axios.post(`${process.env.REACT_APP_API_BASE_URL}/sign-in`, body);
     request.then(response => {
-      toast.success("Seu cadastro foi concluído com sucesso", close);
+      setUser(response.data);
+      const userSerial = JSON.stringify(response.data);
+      localStorage.setItem("user", userSerial);
+      history.push("/barbecues");
       setLoading(false);
-      history.push("/");
     });
     request.catch(error => {
-      toast.error("Não foi possível realizar o cadastro. Por favor, preencha os campos corretamente", close);
+      toast.error("Email ou senha incorretos", close);
       setLoading(false);
     });
   }
@@ -42,7 +48,7 @@ export default function SignIn() {
     <Background>
       <Title />
       <Form onSubmit={login}>
-        <fieldset disabled={`${loading ? "disabled" : "" }`}>
+        <fieldset disabled={`${loading ? "disabled" : ""}`}>
           <span>
             <h1>Faça o seu login</h1>
           </span>
@@ -55,7 +61,7 @@ export default function SignIn() {
             onChange={e => setPassword(e.target.value)}
           />
           <Button type="submit" color="primary" disabled={loading} location={location}>
-          Entrar
+            Entrar
           </Button>
         </fieldset>
       </Form>
