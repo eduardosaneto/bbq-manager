63a28ebc7b987290152e99c4f9d6f41ee4d7da6d
Eduardo Sá Neto <eduardo.albuquerque94@gmail.com>
2021-09-07 02:52:30 -0300

    style: change on add person button

--- src/components/Barbecues/SelectedBarbecue/BarbecueTitle.js
+++ src/components/Barbecues/SelectedBarbecue/BarbecueTitle.js
@@ -1,21 +1,21 @@
 import styled from "styled-components";
-import { IoAddCircle } from "react-icons/io5";
 
 export default function BarbecueTitle({ id, name }) {
   return (
     <Box>
       <h1>{name}</h1>
-      <AddIcon id={id}/>
+      <button>Adicione uma pessoa</button>
     </Box>
   );
 }
 
 const Box = styled.div`
   width: 100%;
-  height: 40px;
+  height: 100px;
   padding: 10px;
   margin-bottom: 15px;
   display: flex;
+  flex-direction: column;
   justify-content: space-between;
   align-items: center;
 
@@ -25,9 +25,19 @@ const Box = styled.div`
     line-height: 32px;
     color: #000;
   }
-`;
 
-const AddIcon = styled(IoAddCircle)`
-  font-size: 35px;
-  color: #ffd836;
+  button {
+    width: 100%;
+    height: 50px;
+    margin-top: 15px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background: #ffd836;
+    box-shadow: 0px 0px 16px rgba(0, 0, 0, 0.06);
+    border-radius: 18px;
+    font-size: 18px;
+    font-weight: 700;
+    color: #fff;
+  }
 `;
